79e7b359be39da4a57fc32c6c98ae376059a89f7
Jeny-Patel <114536084+Jeny-Patel@users.noreply.github.com>
2023-11-06 09:35:39 -0500

    File that contains Lab 5 requirements
    It contains add, delete, change database product.

--- MainActivity.java
+++ MainActivity.java
@@ -1,140 +1,101 @@
-package com.example.simplecalculator;
+package com.example.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView resultText;
+    DatabaseReference databaseProducts;
 
-    private enum Operator {none, add, sub, mul, div, eq}
-
-    private double data01 = 0, data02 = 0, result=0;
-
-    private Operator opp = Operator.none;
-
-    private boolean hasDot = false;
-
-    private boolean requiresCleaning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        resultText = (TextView) findViewById(R.id.resultText);
-
+        databaseProducts = FirebaseDatabase.getInstance().getReference("Products");
     }
 
-    // Sample implementation of the onClickNumericalButton (...).
-    // Feel free to re-implement or modidy.
-    public void onClickNumericalButton(View view) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseProducts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                products.clear();
 
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
 
-        //Getting ID of pressed Button
-        int pressID = view.getId();
+                    Product product = postSnapshot.getValue(Product.class);
+                    Products.add(product);
 
-        //If we had an equal sign pressed last, standard operation is to clean
-        if (opp == Operator.eq) {
-            opp = Operator.none;
-            resultText.setText("");
-        }
-
-        if (requiresCleaning) {
-            requiresCleaning = false;
-            hasDot = false;
-            resultText.setText("");
-        }
-
-        //Figuring out which button was pressed and updating the represented text field object
-        if (pressID == R.id.button0) {
-            resultText.setText(resultText.getText() + "0");
-        } else if (pressID == R.id.button1) {
-            resultText.setText(resultText.getText() + "1");
-        } else if (pressID == R.id.button2) {
-            resultText.setText(resultText.getText() + "2");
-        } else if (pressID == R.id.button3) {
-            resultText.setText(resultText.getText() + "3");
-        } else if (pressID == R.id.button4) {
-            resultText.setText(resultText.getText() + "4");
-        } else if (pressID == R.id.button5) {
-            resultText.setText(resultText.getText() + "5");
-        } else if (pressID == R.id.button6) {
-            resultText.setText(resultText.getText() + "6");
-        } else if (pressID == R.id.button7) {
-            resultText.setText(resultText.getText() + "7");
-        } else if (pressID == R.id.button8) {
-            resultText.setText(resultText.getText() + "8");
-        } else if (pressID == R.id.button9) {
-            resultText.setText(resultText.getText() + "9");
-        } else if (pressID == R.id.buttonDot) {
-            if (!hasDot) {
-                resultText.setText(resultText.getText() + ".");
-                hasDot = true;
-            }
-        } else {
-            resultText.setText("ERROR");
-        }
-
-    }
-
-    public void onClickFunctionButton(View view) {
-
-        int pressID = view.getId();
-
-
-
-        if (pressID != R.id.buttonEq) {
-            data01 = Double.parseDouble(resultText.getText().toString());
-        }
-
-        if (pressID == R.id.buttonPlus) {
-            resultText.setText("");
-            opp = Operator.add;
-        } else {
-            if (pressID == R.id.buttonMinus) {
-                resultText.setText("");
-                opp = Operator.sub;
-
-            } else {
-                if (pressID == R.id.buttonDiv) {
-                    resultText.setText("");
-                    opp = Operator.div;
-                } else {
-                    if (pressID == R.id.buttonMul) {
-                        resultText.setText("");
-                        opp = Operator.mul;
-                    } else {
-                        if (pressID == R.id.buttonEq) {
-                            data02 = Double.parseDouble(resultText.getText().toString());
-                            if (opp == Operator.add) {
-                                result = data01 + data02;
-                            } else {
-                                if (opp == Operator.div) {
-                                    result = data01 / data02;
-                                } else {
-                                    if (opp == Operator.mul) {
-                                        result = data01 * data02;
-                                    } else {
-                                        if (opp == Operator.sub) {
-                                            result = data01 - data02;
-                                        }
-                                    }
-                                    resultText.setText(String.valueOf(result));
-                                    data02 = 0;
-                                    data01 = 0;
-                                }
-                            }
-                        }
-                    }
                 }
 
+                ProductList productAdapter = new ProductList(MainActivity.this,products);
+                listViewProducts.setAdapter(productsAdapter);
+
 
             }
 
-        }
-    }
+            private void addProduct(){
+                String name = editTextName.getText().toString().trim();
+                double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
 
+                if(!TextUtils.isEmpty(name)){
+                    String id = databaseProducts.push().getKey();
+                    Product product = new Product(id,name,price);
+                    databaseProducts.child(id).setValue(product);
+                    editTextName.setText("");
+                    editTextPrice.setText("");
+
+                    Toast.makeText(this,"Product added",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this,"Please enter a name", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            private void updateProduct(String id, String name, double price){
+                DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+                Product product = new Product(id, name, price);
+                dR.setValue(product);
+                Toast.makeText(getApplicationContext(),"Product Updated", Toast.LENGTH_LONG).show();
+            }
+
+            private boolean deleteProduct(String id){
+                DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+                dR.removeValue();
+                Toast.makeText(getApplicationContext(),"Product Deleted",Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
+    }
 }
